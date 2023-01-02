0f3c113e222d1abc2f30b5322fe3c0fb9b2af212
Andrew <MoonHeiz@yandex.by>
2023-01-02 12:11:31 +0300

    feat: add deploy to pipeline, manifest and html template, app icons

--- apps/web/src/components/header/header.tsx
+++ apps/web/src/components/header/header.tsx
@@ -8,7 +8,7 @@ function Header() {
     <header className={styles.header}>
       <Link className={styles.container} href="/">
         <div className={styles.logo} />
-        <h1 className={styles.appName}>Watch together</h1>
+        <h1 className={styles.appName}>Watch Together</h1>
       </Link>
       <MiniAccount />
     </header>
