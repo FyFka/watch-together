f23b4b6e01278393a54ff531508af0f44ba4d723
Andrew <MoonHeiz@yandex.by>
2023-01-09 19:10:43 +0300

    fix: server side player rendering

--- apps/web/src/routes/room/room.tsx
+++ apps/web/src/routes/room/room.tsx
@@ -1,15 +1,16 @@
 import { h } from "preact";
-import { Fragment, useEffect } from "preact/compat";
+import { Fragment, lazy, Suspense, useEffect } from "preact/compat";
 import Chat from "../../components/chat/chat";
 import Controls from "../../components/controls/controls";
 import { IRoom } from "types/src/Room";
 import { joinRoom, leaveRoom, subscribeToJoinRoom } from "../../api/room";
 import { IExternalEvent } from "types/src/ExternalEvent";
-import Player from "../../components/player/player";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { selectRoom, setRoom } from "../../store/room/roomSlice";
 import Loader from "../../components/loader/loader";
 import styles from "./room.styles.css";
+
+const Player = lazy(() => import("../../components/player/player"));
 
 interface IRoomProps {
   roomId: string;
@@ -43,7 +44,9 @@ function Room({ roomId }: IRoomProps) {
       {room && (
         <Fragment>
           <div className={styles.view}>
-            <Player selectedSource={room.selectedSource} player={room.player} roomId={roomId} />
+            <Suspense fallback={<div>loading.....</div>}>
+              <Player selectedSource={room.selectedSource} player={room.player} roomId={roomId} />
+            </Suspense>
             <Controls sources={room.sources} selectedSource={room.selectedSource} roomId={roomId} />
           </div>
           <Chat chatHistory={room.chatHistory} />
